cbcbb8c5640d0459bdc2510f7f19b02fe419e6bd
tavoli-rgb <61290213+tavoli-rgb@users.noreply.github.com>
2024-10-24 11:31:40 +0200

    sdfg

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,15 @@ FROM python:3.8-slim
 
 WORKDIR /code
 
+# Install git
+RUN apt-get update && apt-get install -y git && apt-get clean
+
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 
-COPY . .
-
-CMD ["python", "app.py"]# Add entrypoint script
+# Add entrypoint script
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
-CMD ["python", "app.py"]
+CMD ["python", "web/app.py"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 # Exit immediately if a command exits with a non-zero status
 set -e
+
 # Set default branch to main if not provided
 REPO_BRANCH=${REPO_BRANCH:-main}
+
 # Clone or pull the repository
-if [ ! -d .git ]; then
-    # Leeren Sie das Verzeichnis, bevor Sie das Repository klonen
-    find . -mindepth 1 -delete
-    git clone -b ${REPO_BRANCH} ${REPO_URL} .
+if [ ! -d web/.git ]; then
+    # Leeren Sie das web-Verzeichnis, bevor Sie das Repository klonen
+    find web -mindepth 1 -delete
+    git clone -b ${REPO_BRANCH} --single-branch ${REPO_URL} web
 else
-    git -C . pull origin ${REPO_BRANCH}
+    git -C web pull origin ${REPO_BRANCH}
 fi
+
 # Start the Flask application
 exec "$@"
