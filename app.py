80795a90dc091cbcac930fb65a0d1509d0cd7317
tavoli-rgb <61290213+tavoli-rgb@users.noreply.github.com>
2024-10-24 14:20:04 +0200

    fix suche

--- app.py
+++ app.py
@@ -30,30 +30,6 @@ def index():
     conn.close()
     return render_template('index.html', shelves=shelves)
 
-@app.route('/assign', methods=['POST'])
-def assign():
-    project_number = request.form['project_number']
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute('SELECT id FROM boxes WHERE project_number IS NULL LIMIT 1')
-    box = cursor.fetchone()
-    if box:
-        cursor.execute('UPDATE boxes SET project_number = %s WHERE id = %s', (project_number, box[0]))
-        conn.commit()
-    cursor.close()
-    conn.close()
-    return redirect(url_for('index'))
-
-@app.route('/remove/<int:box_id>')
-def remove(box_id):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute('UPDATE boxes SET project_number = NULL WHERE id = %s', (box_id,))
-    conn.commit()
-    cursor.close()
-    conn.close()
-    return redirect(url_for('index'))
-
 @app.route('/search', methods=['GET'])
 def search():
     project_number = request.args.get('project_number')
