33ff16451c5d7ad24b26d9b63f3e54a12fa0a512
tavoli-rgb <61290213+tavoli-rgb@users.noreply.github.com>
2024-10-24 09:41:31 +0200

    start

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import os
+import subprocess
 
 app = Flask(__name__)
 
@@ -8,5 +9,10 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    os.system('git pull origin main')
+    repo_url = os.getenv('REPO_URL')
+    if repo_url:
+        if not os.path.exists('.git'):
+            subprocess.run(['git', 'clone', repo_url, '.'])
+        else:
+            subprocess.run(['git', 'pull', 'origin', 'main'])
     app.run(host='0.0.0.0')
