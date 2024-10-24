e47428d10fb0304dfa6ecb00d446526cd4adcca0
tavoli-rgb <61290213+tavoli-rgb@users.noreply.github.com>
2024-10-24 10:13:34 +0200

    pull angepasse

--- web/app.py
+++ web/app.py
@@ -1,18 +1,10 @@
 from flask import Flask
-import os
-import subprocess
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Das ist ein Test!'
+    return 'Hello, World!'
 
 if __name__ == '__main__':
-    repo_url = os.getenv('REPO_URL')
-    if repo_url:
-        if not os.path.exists('.git'):
-            subprocess.run(['git', 'clone', repo_url, '.'])
-        else:
-            subprocess.run(['git', 'pull', 'origin', 'main'])
     app.run(host='0.0.0.0')
