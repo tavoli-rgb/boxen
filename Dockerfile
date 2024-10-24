8b85e609bed584658e56ac7428e76e06ddc13b71
tavoli-rgb <61290213+tavoli-rgb@users.noreply.github.com>
2024-10-24 11:18:42 +0200

    änderungen

--- Dockerfile
+++ Dockerfile
@@ -2,15 +2,12 @@ FROM python:3.8-slim
 
 WORKDIR /code
 
-# Install git
-RUN apt-get update && apt-get install -y git && apt-get clean
-
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 
 COPY . .
 
-# Add entrypoint script
+CMD ["python", "app.py"]# Add entrypoint script
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
-
 # Exit immediately if a command exits with a non-zero status
 set -e
-
 # Set default branch to main if not provided
 REPO_BRANCH=${REPO_BRANCH:-main}
-
 # Clone or pull the repository
 if [ ! -d .git ]; then
+    # Leeren Sie das Verzeichnis, bevor Sie das Repository klonen
+    find . -mindepth 1 -delete
     git clone -b ${REPO_BRANCH} ${REPO_URL} .
 else
     git -C . pull origin ${REPO_BRANCH}
 fi
-
 # Start the Flask application
 exec "$@"
