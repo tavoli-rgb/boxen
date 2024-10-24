aa79ee5d4def1e6d12895fd20a9344b604f5efbd
tavoli-rgb <61290213+tavoli-rgb@users.noreply.github.com>
2024-10-24 13:21:57 +0200

    --app nicht kompatiebel

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,5 +12,8 @@ cp -r $TEMP_DIR/app.py $TEMP_DIR/templates /code/
 # Clean up the temporary directory
 rm -rf $TEMP_DIR
 
+# Set the FLASK_APP environment variable
+export FLASK_APP=/code/app.py
+
 # Start the Flask app
-exec flask run --host=0.0.0.0 --port=5000 --app app:app
+exec flask run --host=0.0.0.0 --port=5000
