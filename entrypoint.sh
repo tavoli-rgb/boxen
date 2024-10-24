ebfc6898f3037e0a55839092f7fc6687cb5ebed2
tavoli-rgb <61290213+tavoli-rgb@users.noreply.github.com>
2024-10-24 12:41:28 +0200

    dghj

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-# Clone or pull the latest code from the repository
-if [ ! -d "/code/.git" ]; then
-  git clone -b $REPO_BRANCH $REPO_URL /code
-else
-  cd /code
-  git pull origin $REPO_BRANCH
-fi
+# Define a temporary directory
+TEMP_DIR=$(mktemp -d)
+
+# Clone the latest code from the repository into the temporary directory
+git clone -b $REPO_BRANCH $REPO_URL $TEMP_DIR
+
+# Copy the relevant files to the /code directory
+cp -r $TEMP_DIR/app.py $TEMP_DIR/templates /code/
+
+# Clean up the temporary directory
+rm -rf $TEMP_DIR
 
 # Start the Flask app
 exec flask run --host=0.0.0.0 --port=5000
