90d69b22d3776d63da87b226acf385b344664149
tavoli-rgb <61290213+tavoli-rgb@users.noreply.github.com>
2024-10-24 10:17:48 +0200

    neuer test

--- web/app.py
+++ web/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Das ist ein neuer TEST'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
